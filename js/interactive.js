b9d3d78eb9cd7e849c22c62cae8a93552b5e7c1a
Eugene UST <yliujz@connect.ust.hk>
2025-09-26 22:14:55 +0800

    change at 2214 20250926

---
+++ js/interactive.js
@@ -0,0 +1,7 @@
+function showHidden ( password ) {
+    if ( password == 'eugenel9929' ) {
+        let navElement = document.querySelector('header nav ul');
+        navElement.innerHTML += '<li> <a href="index_.html"> Private Space </a> </li>'
+    }
+    print('failed!');
+}
